32822f92e80acee5e3df724381ecea66272d3e0a
maxciolfi <78815944+maxciolfi@users.noreply.github.com>
2025-10-11 18:07:47 +0200

    Update bypass.cmd

--- bypass.cmd
+++ bypass.cmd
@@ -1,2 +1,2 @@
-curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/maxciolfi/bypass/refs/heads/main/unattend.xml
+curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/maxciolfi/bypass/refs/heads/main/unattend.xml?token=GHSAT0AAAAAADM55FEEB35KWTJLWDONA2UK2HKQDNA
 %WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
